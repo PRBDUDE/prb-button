1325e07cf8a244686b38052963c535a7b4cb8b84
PRBDUDE <programmerroadblocks@gmail.com>
2026-04-09 14:31:02 -0400

    add theme files

---
+++ src/app/themes/colors/violet.ts
@@ -0,0 +1,27 @@
+export const Violet = {
+  50: "#F9F7FF",
+  100: "#E3D8FD",
+  200: "#CDB9FB",
+  300: "#B79AF9",
+  400: "#A17BF8",
+  500: "#8B5CF6",
+  600: "#764ED1",
+  700: "#6140AC",
+  800: "#4C3387",
+  900: "#382562",
+  950: "#23173E",
+}
+
+export const VioletSemantic = {
+  50: "{violet.50}",
+  100: "{violet.100}",
+  200: "{violet.200}",
+  300: "{violet.300}",
+  400: "{violet.400}",
+  500: "{violet.500}",
+  600: "{violet.600}",
+  700: "{violet.700}",
+  800: "{violet.800}",
+  900: "{violet.900}",
+  950: "{violet.950}",
+}
