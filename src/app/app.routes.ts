8c22b78ce9f5f1a2dbb50f49b72df5884cd8de32
PRBDUDE <programmerroadblocks@gmail.com>
2026-04-09 14:44:12 -0400

    add demo routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,76 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./features/home/home')
+      .then(m => m.Home),
+    title: 'Home'
+  },
+  {
+    path: 'contrast',
+    loadComponent: () => import('./features/contrast-demo/contrast-demo')
+      .then(m => m.ContrastDemo),
+    title: 'Contrast Demo'
+  },
+  {
+    path: 'danger',
+    loadComponent: () => import('./features/danger-demo/danger-demo')
+      .then(m => m.DangerDemo),
+    title: 'Danger Demo'
+  },
+  {
+    path: 'help',
+    loadComponent: () => import('./features/help-demo/help-demo')
+      .then(m => m.HelpDemo),
+    title: 'Help Demo'
+  },
+  {
+    path: 'information',
+    loadComponent: () => import('./features/information-demo/information-demo')
+      .then(m => m.InformationDemo),
+    title: 'Information Demo'
+  },
+  {
+    path: 'primary',
+    loadComponent: () => import('./features/primary-demo/primary-demo')
+      .then(m => m.PrimaryDemo),
+    title: 'Primary Demo'
+  },
+  {
+    path: 'secondary',
+    loadComponent: () => import('./features/secondary-demo/secondary-demo')
+      .then(m => m.SecondaryDemo),
+    title: 'Secondary Demo'
+  },
+  {
+    path: 'success',
+    loadComponent: () => import('./features/success-demo/success-demo')
+      .then(m => m.SuccessDemo),
+    title: 'Success Demo'
+  },
+  {
+    path: 'tertiary',
+    loadComponent: () => import('./features/tertiary-demo/tertiary-demo')
+      .then(m => m.TertiaryDemo),
+    title: 'Tertiary Demo'
+  },
+  {
+    path: 'plain',
+    loadComponent: () => import('./features/plain-demo/plain-demo')
+      .then(m => m.PlainDemo),
+    title: 'Plain Demo'
+  },
+  {
+    path: 'warning',
+    loadComponent: () => import('./features/warning-demo/warning-demo')
+      .then(m => m.WarningDemo),
+    title: 'Warning Demo'
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./features/not-found/not-found')
+      .then(m => m.NotFound),
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Menu} from './features/menu/menu';
 
 @Component({
   selector: 'prb-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
